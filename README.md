973bf5266762cf4a31002ff7ed3139bb161cf57b
Lazu George Mihai <lazugeorgem@gmail.com>
2019-06-30 20:10:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## swagger-to-static
+
+Convert a JSON or YML swagger spec to a static HTML file.
+
+### Usage
+
+### Credits
+This is merely a js port of [this gist](https://gist.github.com/oseiskar/dbd51a3727fc96dcf5ed189fca491fb3). All credits to [this](https://github.com/oseiskar) guy.
---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+'use strict';
+
+const ArgumentParser = require("argparse").ArgumentParser;
+const yaml = require('js-yaml');
+const path = require("path");
+const fs = require("fs");
+
+const template = fs.readFileSync("template.html");
+const parser = new ArgumentParser({
+    version: "0.0.1",
+    addHelp: true,
+});
+
+parser.addArgument([ "FILE" ],  { help: "Input spec, as JSON or YAML" });
+parser.addArgument([ "TARGET" ],  { help: "Output directory" });
+const args = parser.parseArgs();
+
+const fileType = path.extname(args.FILE);
+let swaggerSpec = null;
+
+switch (fileType.toLowerCase()) {
+    case ".json":
+        swaggerSpec = fs.readFileSync(args.FILE, "utf8").toJSON();
+        break;
+    case ".yml":
+        const yml = fs.readFileSync(args.FILE, "utf8");
+        swaggerSpec = yaml.safeLoad(yml);
+        break;
+    default:
+        console.error(`File type ${fileType} not supported. Supported file types: json, yml`);
+        process.exit(-1);
+}
+
+const output = template.toString().replace("{{{spec}}}", JSON.stringify(swaggerSpec));
+fs.writeFileSync(`${args.TARGET}/index.html`, output);
